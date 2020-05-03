a91f02701984fccb2798831062b6c7771af2fd7e
nocturnalarchives <64667374+nocturnalarchives@users.noreply.github.com>
2020-05-02 21:14:00 -0400

    .

--- include-pihole-blocklist-html.php
+++ include-pihole-blocklist-html.php
@@ -1,8 +1,8 @@
 <h1>PiHole Blocklists</h1>
 <ul>
-  <li><strong><a href="" target="_blank">Big Tech Advertisers and Trackers</a></strong> - Aggresivley blocks all the Big Tech Trackers.</li>
-    <li><strong><a href="" target="_blank">Apple Advertisers and Trackers</a></strong></li>
-  <li><strong><a href="" target="_blank">Social Media Advertisers and Trackers</a></strong> - Facebook, Instagram, Whatsapp and other miscellaneous streaming services advertisers and trackers.</li>
+  <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/tech-trackers.txt" target="_blank">Big Tech Advertisers and Trackers</a></strong> - Aggresivley blocks all the Big Tech Trackers.</li>
+    <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/apple-trackers.txt" target="_blank">Apple Advertisers and Trackers</a></strong></li>
+  <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/social-media-trackers.txt" target="_blank">Social Media Advertisers and Trackers</a></strong> - Facebook, Instagram, Whatsapp and other miscellaneous streaming services advertisers and trackers.</li>
     <ul>
       <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/facebook-trackers.txt" target="_blank">Facebook Advertisers and Trackers</a></strong></li>
     </ul>
@@ -12,10 +12,10 @@
       <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/disney-plus-trackers.txt" target="_blank">Disney+ Advertisers and Trackers</a></strong></li>
       <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/netflix-trackers.txt" target="_blank">Netflix Trackers</a></strong></li>
     </ul>
-
-
     <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/sonos-metrics.txt" target="_blank">Sonos Trackers</a></strong> - Sonos listening data and metrics trackers.</li>
     <li><strong><a href="https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/sonos-updates.txt" target="_blank">Sonos Update Domains and Subdomains</a></strong> - This list will block your Sonos eqipment from recieving updates.</li>
+
+    
     <li><strong><a href="" target="_blank">List Name Advertisers and Trackers</a></strong> - description here.</li>
     <li><strong><a href="" target="_blank">List Name Advertisers and Trackers</a></strong> - description here.</li>
     <li><strong><a href="" target="_blank">List Name Advertisers and Trackers</a></strong> - description here.</li>
